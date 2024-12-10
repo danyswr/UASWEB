03a1bcb0c5a29b8bb398e236c7132c12147fcd2a
hannartcraft <hannauliyaa@gmail.com>
2024-12-10 14:22:59 +0700

    kocak

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Web RPG</title>
+        <title>Web RPG kocak</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
