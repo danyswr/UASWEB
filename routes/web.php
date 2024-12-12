e76aba2f27137cdb36bc644ad92b09b7dbc123e8
danyswr <daniswara.173@gmail.com>
2024-12-13 00:44:17 +0700

    gw udh perbarui dengan desain frontend nya

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\BattleController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Login');
+})->name('login');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard')->middleware(['auth']);
+
+// Battle routes
+Route::middleware(['auth'])->group(function () {
+    Route::get('/battle/start/{monsterId}', [BattleController::class, 'startBattle']);
+    Route::post('/battle/attack/{battleId}', [BattleController::class, 'attack']);
+    Route::post('/battle/defend/{battleId}', [BattleController::class, 'defend']);
+    Route::get('/battle/end/{battleId}', [BattleController::class, 'endBattle']);
 });
 
-Route::get('/battle/start/{monsterId}', 'BattleController@startBattle')->middleware('auth');
-Route::post('/battle/attack/{battleId}', 'BattleController@attack')->middleware('auth');
-Route::post('/battle/defend/{battleId}', 'BattleController@defend')->middleware('auth');
-Route::get('/battle/end/{battleId}', 'BattleController@endBattle')->middleware('auth');
